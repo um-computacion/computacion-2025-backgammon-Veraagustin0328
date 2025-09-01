56e130fad1251722b0ecbdb2323552df4cff8e4f
Your Name <you@example.com>
2025-08-31 21:39:22 -0300

    Agregue la clase Dice

---
+++ backgammon/backgammon/core/dice.py
@@ -0,0 +1,19 @@
+import random
+
+class Dice:
+    
+    def __init__(self, rng=None):
+        self.__rng__ = rng or random.Random()
+        self.__ultima_tirada__ = (0, 0)
+
+    def get_ultima_tirada(self):
+        return self.__ultima_tirada__
+
+    def set_rng(self, rng):
+        self.__rng__ = rng
+
+    def roll(self):
+        a = self.__rng__.randint(1, 6)
+        b = self.__rng__.randint(1, 6)
+        self.__ultima_tirada__ = (a, b)
+        return self.__ultima_tirada__
